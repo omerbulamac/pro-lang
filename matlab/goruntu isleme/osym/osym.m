5e8698298e95671e905753ddb8ad06bdfd53040b
Hasan Arslan <cehars@bil.omu.edu.tr>
2012-12-07 02:04:06 +0200

    vize_cevaplari

--- matlab/goruntu isleme/osym/osym.m
+++ matlab/goruntu isleme/osym/osym.m
@@ -5,8 +5,34 @@ function osym(resim)
     sz = size(I);
     
     I = rgb2gray(I);
-    I = im2bw(I)
-   imshow(I) 
     
+    
+    for i=1:sz(1)
+        for j=1:sz(2)
+            if I(i,j) == 0 || I(i,j) == 255
+                I(i,j) = 0;
+            else
+                I(i,j) = 255;
+            end
+        end
+    end
+    
+   [etiket, sayi] = bwlabel(I);
+   osym_logosu = etiket==1
+   
+    s = regionprops(osym_logosu, 'centroid')
+    merkez = s.Centroid
+    
+    if (merkez(1) < sz(2) / 2) && (merkez(2) > sz(1) / 2)
+        fprintf('0 derece\n');
+    elseif (merkez(1) > sz(2) / 2) && (merkez(2) < sz(1) / 2)
+        fprintf('180 derece\n');
+	elseif (merkez(1) < sz(2) / 2) && (merkez(2) < sz(1) / 2)
+        fprintf('270 derece\n');
+    elseif (merkez(1) > sz(2) / 2) && (merkez(2) > sz(1) / 2)
+        fprintf('90 derece\n');
+    end
     
 end
+
+% cehars
