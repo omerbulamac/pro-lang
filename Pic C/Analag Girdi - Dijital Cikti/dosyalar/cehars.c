725393edf6b8835fd2f6cea12871adbe2d9456e5
Hasan Arslan <cehars@bil.omu.edu.tr>
2012-12-11 00:24:49 +0200

    pic c

--- Pic C/Analag Girdi - Dijital Cikti/dosyalar/cehars.c
+++ Pic C/Analag Girdi - Dijital Cikti/dosyalar/cehars.c
@@ -8,24 +8,22 @@ void main( )
 { 
    byte X;
    setup_ccp1(CCP_PWM); // CCP1 PWM moduna ayarlanýyor
-   setup_timer_2(T2_DIV_BY_16, 255, 16); // Periyot (1/clock)*4*t2div*(period+1)
-   setup_port_a(ALL_ANALOG); // Fosc=20000000 ve periyot katsayýsý=127
-   setup_adc(adc_clock_internal); // (1/20000000)*4*16*128= 409,6 us or 2,44 khz
+   setup_timer_2(T2_DIV_BY_16, 255, 16); // Analog sinyali 255 parçaya bölüyor.
+   setup_port_a(ALL_ANALOG); // A portunun hepsini analog girdi yaptýk
+   setup_adc(adc_clock_internal); 
    
    
    while( TRUE )
    {
    
-   set_adc_channel( 0 );
+   set_adc_channel( 0 ); // Pic'in RA0 bacaðýndan analog giriþ al
    delay_us(20);
-   x=read_adc();
+   x=read_adc(); // Aldýðýn giriþi x deðiþkenine ata
    
- 
-   
-   if (x>128)
+   if (x>128)        //  Eðer 255 parçaya bölünen analog sinyal 128 den büyükse b0 numaralý pini dijital 1 yap          
    output_high(pin_d0);
    
-   else 
+   else              // Deðilse dijital 0 yap
    output_low(pin_d0);
    
    
--- Pic C/Dijital Girdi  - Dijital Cikti/dosyalar/dijital.c
+++ Pic C/Dijital Girdi  - Dijital Cikti/dosyalar/dijital.c
@@ -3,28 +3,29 @@
 #fuses XT, NOWDT, NOPROTECT, NOBROWNOUT, NOLVP, NOPUT, NOWRT, NODEBUG, NOCPD
 
 
-#use fast_io(a)
-#use fast_io(b)
+#use fast_io(a) // port yönlendirme komutlarý A portu için geçerli
+#use fast_io(b) // port yönlendirme komutlarý B portu için geçerli
 
 int1 x;
   
 void main()
   {
-  setup_psp(PSP_DISABLED);
-  setup_timer_1(T1_DISABLED);
-   setup_timer_2(T2_DISABLED,0,1);
-   setup_adc_ports(NO_ANALOGS);
-  setup_adc(ADC_OFF);
-  setup_CCP1(CCP_OFF);
-  setup_CCP2(CCP_OFF);
+  setup_psp(PSP_DISABLED);   // PSP birimi devre dýþý
+  setup_timer_1(T1_DISABLED);   // T1 zamanlayýcýsý devre dýþý
+  setup_timer_2(T2_DISABLED,0,1);   // T2 zamanlayýcýsý devre dýþý
+  setup_adc_ports(NO_ANALOGS);    // ANALOG giriþ yok
+  setup_adc(ADC_OFF);    // ADC birimi devre dýþý
+  setup_CCP1(CCP_OFF);   // CCP1 birimi devre dýþý
+  setup_CCP2(CCP_OFF);   // // CCP2 birimi devre dýþý
   
-  set_tris_a(0b00000001);
-  set_tris_b(0x00);
+  set_tris_a(0b00000001); // RA0 ucu giriþ olarak ayarlandý.
+  set_tris_b(0x00);   // RB0 portu tümüyle çýkýþ olarak ayarlandý.
   
   basla:
-  x = input(pin_a0);
   
-  output_bit(pin_b0,x);
+  x = input(pin_a0);      // RA0 giriþi okunuyor ve c deðiþkenine aktarýlýyor.
+  output_bit(pin_b0,x);   //   x deðiþkeninin deðeri RB0 pini çýkýþýna aktarýlýyor.
+ 
   goto basla;
   
   }
