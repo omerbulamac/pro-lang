60bbd68fdb64f2e2ec07ab8f1c3b9bb3b04c0b2e
cehars <cehars@bil.omu.edu.tr>
2013-05-10 11:44:39 +0300

    t-ff

--- verilog/t_flip-flop-2.v
+++ verilog/t_flip-flop-2.v
@@ -6,13 +6,9 @@ output q ;
 reg pst ;
 wire nst ;
 
-// sonraki durumu input ve şimdiki durum ile tanımla
 assign nst = t ? ~ pst : pst ;
-
-// çıktıyı input ve şimdiki durum cinsinden tanımla
 assign q = pst ;
 
-// hafızayı tanımla (sonraki durum ve şimdiki durum arasındaki geçiş)
 always @ ( posedge ck or negedge rn )
 if ( !rn ) pst <= 0 ;
 else pst <= nst ;
