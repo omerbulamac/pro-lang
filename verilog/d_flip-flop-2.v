a3df10f92a85f8b70711f92353673901a2916b2d
cehars <cehars@bil.omu.edu.tr>
2013-05-10 03:51:41 +0300

    d_ff-2

---
+++ verilog/d_flip-flop-2.v
@@ -0,0 +1,16 @@
+module d_ff ( D, CK, RN, Q);
+ 
+input D , CK , RN ;
+output Q ;
+reg Q ;
+
+always @ ( posedge CK or negedge RN )
+if ( !RN ) Q <= 0 ;
+else Q <= D ;
+
+// CK sinyali yukselen kenardayken veya RN alcalan kenardayken blogtaki kodlarý isle
+// CK yukselen kenari sayesinde bloga girdiyse eger if dongusune takýlmadan D neyse cikisa onu aktaracak
+// ama RN alcalan kenari sayesinde girdiyse donguye if dongusundeki kod parcasi islenecek ve cikisa sifir
+// verilecek.
+
+endmodule
