f2af5388c9923861c0deb3db1a71d725bbcee9cc
cehars <cehars@bil.omu.edu.tr>
2013-06-02 03:25:31 +0300

    .

---
+++ matlab/gui/topla2/topla.m
@@ -0,0 +1,57 @@
+function varargout = topla(varargin)
+
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @topla_OpeningFcn, ...
+                   'gui_OutputFcn',  @topla_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+function topla_OpeningFcn(hObject, eventdata, handles, varargin)
+handles.output = hObject;
+
+guidata(hObject, handles);
+
+function varargout = topla_OutputFcn(hObject, eventdata, handles) 
+varargout{1} = handles.output;
+
+
+
+function edit1_Callback(hObject, eventdata, handles)
+function edit1_CreateFcn(hObject, eventdata, handles)
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit2_Callback(hObject, eventdata, handles)
+function edit2_CreateFcn(hObject, eventdata, handles)
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function edit3_Callback(hObject, eventdata, handles)
+function edit3_CreateFcn(hObject, eventdata, handles)
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function pushbutton1_Callback(hObject, eventdata, handles)
+
+number1 = str2double(get(handles.edit1,'String'));
+number2 = str2double(get(handles.edit2,'String'));
+number3 = number1 + number2;
+msgbox(num2str(number3));
