1fb2cceb91f7ec6e992e0a89815ab01a1c341f65
cehars <cehars@bil.omu.edu.tr>
2013-06-07 01:13:08 +0300

    aciklama

--- arduino/led_yak_sondur/led_yak_sondur.ino
+++ arduino/led_yak_sondur/led_yak_sondur.ino
@@ -1,13 +1,12 @@
 
-int led = 13;
-
 void setup() {                
-  pinMode(led, OUTPUT);     
+  pinMode(13, OUTPUT);   // arduino kartındeki 13. pini çıkış pini yap.  
 }
 
-void loop() {
-  digitalWrite(led, HIGH);   
-  delay(1000);               
-  digitalWrite(led, LOW);    
-  delay(1000);          
+void loop() { // sonsuz döngüye giriliyor.
+
+  digitalWrite(13, HIGH);   // 13. numaralı pin lojiik 1 yapılıyor. 
+  delay(1000);     // 1000 ms bekle          
+  digitalWrite(13, LOW);    // 13. numaralı pin lojiik 0 yapılıyor. 
+  delay(1000);     // 1000 ms bekle               
 }
