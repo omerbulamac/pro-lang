21ae903713f4b1f04e3bf2323453d5ed2fb0cfef
cehars <cehars@bil.omu.edu.tr>
2013-06-08 07:17:44 +0300

    alinan analog sinyali eszamanli cizdirme

--- arduino/matlab/analog_input.m
+++ arduino/matlab/analog_input.m
@@ -1,8 +1,14 @@
 % a = arduino('COM5');
+sayac = 0;
 while (1)
-a.analogRead(8)
-pause(0.5);
+    sayac = sayac + 1;
+t = a.analogRead(8)
+pause(0.1);
+liste(sayac) = t;
+plot(liste);
 end
+
+
 
 % DÝKKAT 
 % matlab arduino etkileþiminde analog okumak biraz farklý 
