3ef0d8c9845f17816187db59295ff5b0a9d22252
cehars <cehars@bil.omu.edu.tr>
2013-06-08 03:23:24 +0300

    matlab

---
+++ arduino/matlab/analog_digital.m
@@ -0,0 +1,20 @@
+% a = arduino('COM5');
+a.pinMode(2,'output');
+
+a.pinMode(11,'output');
+a.pinMode(13, 'output');
+a.digitalWrite(13, 1);
+while(1)
+    int fadeValue;
+    for fadeValue=180:10:250
+    
+    a.digitalWrite(11, 0);
+    a.digitalWrite(2, 1); 
+    pause(0.8);
+    a.digitalWrite(2, 0);
+    a.digitalWrite(11, 1); 
+    pause(0.8);
+    end
+end
+a.digitalWrite(2, 0);
+a.digitalWrite(11, 0);
