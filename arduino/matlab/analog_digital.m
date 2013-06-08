50c00d116c341305138d5bfda8b4d3089ccc2d1a
cehars <cehars@bil.omu.edu.tr>
2013-06-08 03:53:08 +0300

    matlab

--- arduino/matlab/analog_digital.m
+++ arduino/matlab/analog_digital.m
@@ -7,13 +7,14 @@ a.digitalWrite(13, 1);
 while(1)
     int fadeValue;
     for fadeValue=180:10:250
+    a.analogWrite(9, fadeValue);
     
     a.digitalWrite(11, 0);
     a.digitalWrite(2, 1); 
-    pause(0.8);
+    pause(0.3);
     a.digitalWrite(2, 0);
     a.digitalWrite(11, 1); 
-    pause(0.8);
+    pause(0.3);
     end
 end
 a.digitalWrite(2, 0);
