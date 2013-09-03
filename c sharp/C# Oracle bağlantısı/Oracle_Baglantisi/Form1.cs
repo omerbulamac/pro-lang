18d30b44ce1fc423c8405f2ebd379da0271173f6
Hasan Arslan <cehars@bil.omu.edu.tr>
2013-09-04 02:56:12 +0300

    c# oracle baglantisi

---
+++ c sharp/C# Oracle bağlantısı/Oracle_Baglantisi/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Oracle.DataAccess.Client;
+
+namespace Oracle_Baglantisi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OracleConnection conn = new OracleConnection();
+
+            conn.ConnectionString = "User ID=cehars; Password=19; Data Source=PC";
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Veritabanına Baglantı Basarılı.");
+                conn.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Veritabanına Baglanılamadı.");
+            }
+        }
+    }
+}
