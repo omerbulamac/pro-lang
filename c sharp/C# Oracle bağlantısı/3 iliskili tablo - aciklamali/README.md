19a4066dfd7de3750b7c858e11d2e0bdfc36791b
Hasan Arslan <cehars@bil.omu.edu.tr>
2013-09-04 12:32:30 +0300

    c# oracle ile iliskisel veritabani islemleri

---
+++ c sharp/C# Oracle bağlantısı/3 iliskili tablo - aciklamali/README.md
@@ -0,0 +1,23 @@
+##### ogrenci_tablosu
+- ogrenci_id
+- ogrenci_ad
+- ogrenci_bolum
+
+##### bolum_tablosu
+- bolum_id
+- bolum_ad
+- bolum_fakulte
+
+##### fakulte_tablosu
+- fakulte_id
+- fakulte_ad
+
+ogrenci_tablosu'nun ogrenci_bolum alanı bolum_tablosu'nun bolum_id alanına baglı.(ogrenci_bolum foregin key)
+bolum_tablosu'nun bolum_fakulte alanı fakulte_tablosu'nun fakulte_id alanına baglı.(bolum_fakulte foregin key)
+
+Veritabanında kayıtlı her ogrencinin gittiği fakulteleri ogrenmek için C# içinde kullandığımız SQL sorgusu
+
+	SELECT ogrenci_tablosu.ogrenci_ad, fakulte_tablosu.fakulte_ad 
+	FROM ogrenci_tablosu, bolum_tablosu, fakulte_tablosu 
+	WHERE ogrenci_tablosu.ogrenci_bolum = bolum_tablosu.bolum_id 
+	AND bolum_tablosu.bolum_fakulte = fakulte_tablosu.fakulte_id
---
+++ c sharp/C# Oracle bağlantısı/3 iliskili tablo - aciklamali/oracle/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Oracle.DataAccess.Client;
+
+namespace oracle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OracleConnection conn = new OracleConnection();
+
+            conn.ConnectionString = "User ID=cehars; Password=19; Data Source=PC";
+            try
+            {
+                conn.Open();
+                MessageBox.Show("Veritabanına Baglantı Basarılı.");
+                
+            }
+            catch
+            {
+                MessageBox.Show("Veritabanına Baglanılamadı.");
+            }
+
+            OracleCommand cmd = new OracleCommand();
+            cmd.CommandText = "SELECT ogrenci_tablosu.ogrenci_ad, fakulte_tablosu.fakulte_ad FROM ogrenci_tablosu, bolum_tablosu, fakulte_tablosu WHERE ogrenci_tablosu.ogrenci_bolum = bolum_tablosu.bolum_id AND bolum_tablosu.bolum_fakulte = fakulte_tablosu.fakulte_id";
+            cmd.Connection = conn;
+
+            OracleDataAdapter adapter = new OracleDataAdapter();
+            adapter.SelectCommand = cmd;
+
+            DataTable dbListe = new DataTable();
+            adapter.Fill(dbListe);
+            dataGridView1.DataSource = dbListe;
+            conn.Close();
+        }
+    }
+}
